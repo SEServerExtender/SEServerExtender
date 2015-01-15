b7f9ae2dddef35fb36acfcff5bc55574b0f3aa3c
Tyrsis <jay_macaulay@hotmail.com>
2015-01-15 12:58:40 -0500

    Version Update

--- SEServerExtender/Properties/AssemblyInfo.cs
+++ SEServerExtender/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.11.45")]
-[assembly: AssemblyFileVersion("0.2.11.45")]
+[assembly: AssemblyVersion("0.2.11.46")]
+[assembly: AssemblyFileVersion("0.2.11.46")]
