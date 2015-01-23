daa4e2370906e520a5d70d32a3e0e8fee492b4b7
dodexahedron <brandon@dodecatec.com>
2015-01-23 17:39:59 -0500

    Moved IInternalServiceContract to its own file.

---
+++ SEModAPIExtensions/API/IPC/IInternalServiceContract.cs
@@ -0,0 +1,36 @@
+﻿namespace SEModAPIExtensions.API.IPC
+{
+	using System.Collections.Generic;
+	using System.ServiceModel;
+	using SEModAPIInternal.API.Entity;
+	using SEModAPIInternal.API.Entity.Sector.SectorObject;
+	using SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid;
+
+	[ServiceContract]
+	public interface IInternalServiceContract
+	{
+		[OperationContract]
+		List<ulong> GetConnectedPlayers();
+
+		[OperationContract]
+		string GetPlayerName(ulong steamId);
+
+		[OperationContract]
+		List<BaseEntity> GetSectorEntities();
+
+		[OperationContract]
+		List<CubeGridEntity> GetSectorCubeGridEntities();
+
+		[OperationContract]
+		List<CubeBlockEntity> GetCubeBlocks(long cubeGridEntityId);
+
+		[OperationContract]
+		List<InventoryItemEntity> GetInventoryItems(long cubeGridEntityId, long containerBlockEntityId, ushort inventoryIndex);
+
+		[OperationContract]
+		void UpdateEntity(BaseEntity entity);
+
+		[OperationContract]
+		void UpdateCubeBlock(CubeGridEntity parent, CubeBlockEntity cubeBlock);
+	}
+}
--- SEModAPIExtensions/API/IPC/IService.cs
+++ SEModAPIExtensions/API/IPC/IService.cs
@@ -32,32 +32,4 @@ namespace SEModAPIExtensions.API.IPC
 		[WebGet(UriTemplate = "GetCubeBlocks/{cubeGridEntityId}")]
 		List<CubeBlockEntity> GetCubeBlocks(string cubeGridEntityId);
 	}
-
-	[ServiceContract]
-	public interface IInternalServiceContract
-	{
-		[OperationContract]
-		List<ulong> GetConnectedPlayers();
-
-		[OperationContract]
-		string GetPlayerName(ulong steamId);
-
-		[OperationContract]
-		List<BaseEntity> GetSectorEntities();
-
-		[OperationContract]
-		List<CubeGridEntity> GetSectorCubeGridEntities();
-
-		[OperationContract]
-		List<CubeBlockEntity> GetCubeBlocks(long cubeGridEntityId);
-
-		[OperationContract]
-		List<InventoryItemEntity> GetInventoryItems(long cubeGridEntityId, long containerBlockEntityId, ushort inventoryIndex);
-
-		[OperationContract]
-		void UpdateEntity(BaseEntity entity);
-
-		[OperationContract]
-		void UpdateCubeBlock(CubeGridEntity parent, CubeBlockEntity cubeBlock);
-	}
 }
