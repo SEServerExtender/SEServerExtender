8fc5f823de5e3dc68b0ca5df4f91282acf223fdc
dodexahedron <brandon@dodecatec.com>
2015-01-23 17:30:23 -0500

    Moved ServerExtensions to its own file.

--- SEModAPIExtensions/API/Server.cs
+++ SEModAPIExtensions/API/Server.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Description;
@@ -814,34 +813,5 @@ namespace SEModAPIExtensions.API
 		}
 
 		#endregion
-	}
-
-	static class ServerExtensions
-	{
-		public static void ClearEventInvocations(this object obj, string eventName)
-		{
-			var fi = obj.GetType().GetEventField(eventName);
-			if (fi == null) return;
-			fi.SetValue(obj, null);
-		}
-
-		private static FieldInfo GetEventField(this Type type, string eventName)
-		{
-			FieldInfo field = null;
-			while (type != null)
-			{
-				/* Find events defined as field */
-				field = type.GetField(eventName, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-				if (field != null && (field.FieldType == typeof(MulticastDelegate) || field.FieldType.IsSubclassOf(typeof(MulticastDelegate))))
-					break;
-
-				/* Find events defined as property { add; remove; } */
-				field = type.GetField("EVENT_" + eventName.ToUpper(), BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic);
-				if (field != null)
-					break;
-				type = type.BaseType;
-			}
-			return field;
-		}
 	}
 }
