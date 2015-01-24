af4d30b608f361152afea146e9810f84d6dae0f3
dodexahedron <brandon@dodecatec.com>
2015-01-23 22:43:30 -0500

    Whitespace

--- SEModAPIExtensions/API/IPC/CorsInspector.cs
+++ SEModAPIExtensions/API/IPC/CorsInspector.cs
@@ -11,52 +11,52 @@ namespace SEModAPIExtensions.API.IPC
 	{
 		Dictionary<string, string> requiredHeaders;
 
-		public CorsInspector(Dictionary<string, string> headers)
+		public CorsInspector( Dictionary<string, string> headers )
 		{
-			requiredHeaders = headers ?? new Dictionary<string, string>();
+			requiredHeaders = headers ?? new Dictionary<string, string>( );
 		}
 
-		public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+		public object AfterReceiveRequest( ref Message request, IClientChannel channel, InstanceContext instanceContext )
 		{
 			return null;
 		}
 
-		public void BeforeSendReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+		public void BeforeSendReply( ref System.ServiceModel.Channels.Message reply, object correlationState )
 		{
-			var httpHeader = reply.Properties["httpResponse"] as HttpResponseMessageProperty;
-			foreach (var item in requiredHeaders)
+			var httpHeader = reply.Properties[ "httpResponse" ] as HttpResponseMessageProperty;
+			foreach ( var item in requiredHeaders )
 			{
-				httpHeader.Headers.Add(item.Key, item.Value);
+				httpHeader.Headers.Add( item.Key, item.Value );
 			}
 		}
 	}
 
-	[AttributeUsage(AttributeTargets.Class)]
+	[AttributeUsage( AttributeTargets.Class )]
 	public class EnableCorsBehavior : Attribute, IServiceBehavior
 	{
-		public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, System.Collections.ObjectModel.Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+		public void AddBindingParameters( ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, System.Collections.ObjectModel.Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters )
 		{
 
 		}
 
-		public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+		public void ApplyDispatchBehavior( ServiceDescription serviceDescription, ServiceHostBase serviceHostBase )
 		{
-			var requiredHeaders = new Dictionary<string, string>();
+			var requiredHeaders = new Dictionary<string, string>( );
 
-			requiredHeaders.Add("Access-Control-Allow-Origin", "*");
-			requiredHeaders.Add("Access-Control-Request-Method", "POST,GET,PUT,DELETE,OPTIONS");
-			requiredHeaders.Add("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+			requiredHeaders.Add( "Access-Control-Allow-Origin", "*" );
+			requiredHeaders.Add( "Access-Control-Request-Method", "POST,GET,PUT,DELETE,OPTIONS" );
+			requiredHeaders.Add( "Access-Control-Allow-Headers", "X-Requested-With,Content-Type" );
 
-			foreach (ChannelDispatcher cd in serviceHostBase.ChannelDispatchers)
+			foreach ( ChannelDispatcher cd in serviceHostBase.ChannelDispatchers )
 			{
-				foreach (EndpointDispatcher ed in cd.Endpoints)
+				foreach ( EndpointDispatcher ed in cd.Endpoints )
 				{
-					ed.DispatchRuntime.MessageInspectors.Add(new CorsInspector(requiredHeaders));
+					ed.DispatchRuntime.MessageInspectors.Add( new CorsInspector( requiredHeaders ) );
 				}
 			}
 		}
 
-		public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+		public void Validate( ServiceDescription serviceDescription, ServiceHostBase serviceHostBase )
 		{
 
 		}
