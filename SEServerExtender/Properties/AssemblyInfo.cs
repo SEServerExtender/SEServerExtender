ed5ce7190ca8b3fcc86c8d61da6c06861c67966c
dodexahedron <brandon@dodecatec.com>
2015-01-26 00:22:22 -0500

    Whitespace only...

--- SEServerExtender/Properties/AssemblyInfo.cs
+++ SEServerExtender/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.11.48")]
-[assembly: AssemblyFileVersion("0.2.11.48")]
+[assembly: AssemblyVersion("0.2.11.49")]
+[assembly: AssemblyFileVersion("0.2.11.49")]
