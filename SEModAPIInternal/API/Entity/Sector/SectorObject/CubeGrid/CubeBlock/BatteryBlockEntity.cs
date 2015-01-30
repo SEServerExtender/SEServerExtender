9bd8e9c4d79c576b1cc0c1e590b7d0860e6db63d
Tyrsis <jay_macaulay@hotmail.com>
2015-01-30 15:14:04 -0500

    Hotfix Update
    - Update game libraries, fixed missing methods

--- SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/BatteryBlockEntity.cs
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/BatteryBlockEntity.cs
@@ -384,8 +384,8 @@ namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
 		//15870 - ProducerEnabled On/Off
 		//15871 - SemiautoEnabled On/Off
 
-		//public static string BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod = "8800B7689C00A9F78830F86A2CBF2BEF";
-		public static string BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod = "011235FA2BA3A6785C1B800F2C3DA7E0";
+		//public static string BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod = "011235FA2BA3A6785C1B800F2C3DA7E0";
+		public static string BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod = "78DE2A1D89F401DA46424A642386FF26";
 
 		public static string BatteryBlockNetManagerCurrentStoredPowerPacketClass = "59DE66D2ECADE0929A1C776D7FA907E2";
 		public static string BatteryBlockNetManagerCurrentStoredPowerPacketValueField = "ADC3AB91A03B31875821D57B8B718AF5";
