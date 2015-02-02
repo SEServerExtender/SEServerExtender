f0984a7252569f82bc12de040d5471af16fa3491
dodexahedron <brandon@dodecatec.com>
2015-02-02 16:28:41 -0500

    Moved BatteryBlockNetworkManager to its own file.

---
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/BatteryBlockNetworkManager.cs
@@ -0,0 +1,176 @@
+namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
+{
+	using System;
+	using System.Reflection;
+	using SEModAPIInternal.API.Common;
+	using SEModAPIInternal.Support;
+
+	public class BatteryBlockNetworkManager
+	{
+		#region "Attributes"
+
+		private BatteryBlockEntity m_parent;
+		private Object m_backingObject;
+
+		private static bool m_isRegistered;
+
+		public static string BatteryBlockNetworkManagerNamespace = BatteryBlockEntity.BatteryBlockNamespace + "." + BatteryBlockEntity.BatteryBlockClass;
+		public static string BatteryBlockNetworkManagerClass = "6704740496C47C5FDE69887798D17883";
+
+		public static string BatteryBlockNetManagerBroadcastProducerEnabledMethod = "280D7AE8C0F523FF089618970C13B55B";
+		public static string BatteryBlockNetManagerBroadcastCurrentStoredPowerMethod = "F512BA7EF29F6A8B7DE3D56BAAC0207B";
+		public static string BatteryBlockNetManagerBroadcastSemiautoEnabledMethod = "72CE36DE9C0BAB6FEADA5D10CF5B867A";
+		public static string BatteryBlockNetManagerCurrentPowerPacketReceiver = "F512BA7EF29F6A8B7DE3D56BAAC0207B";
+
+		///////////////////////////////////////////////////////////////////////
+
+		//Packets
+		//1587 - CurrentStoredPower
+		//1588 - ??
+		//15870 - ProducerEnabled On/Off
+		//15871 - SemiautoEnabled On/Off
+
+		//public static string BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod = "011235FA2BA3A6785C1B800F2C3DA7E0";
+		public static string BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod = "78DE2A1D89F401DA46424A642386FF26";
+
+		public static string BatteryBlockNetManagerCurrentStoredPowerPacketClass = "59DE66D2ECADE0929A1C776D7FA907E2";
+		public static string BatteryBlockNetManagerCurrentStoredPowerPacketValueField = "ADC3AB91A03B31875821D57B8B718AF5";
+
+		#endregion "Attributes"
+
+		#region "Constructors and Initializers"
+
+		public BatteryBlockNetworkManager( BatteryBlockEntity parent, Object backingObject )
+		{
+			m_parent = parent;
+			m_backingObject = backingObject;
+
+			Action action = RegisterPacketHandlers;
+			SandboxGameAssemblyWrapper.Instance.EnqueueMainGameAction( action );
+		}
+
+		#endregion "Constructors and Initializers"
+
+		#region "Properties"
+
+		internal Object BackingObject
+		{
+			get { return m_backingObject; }
+		}
+
+		public static Type InternalType
+		{
+			get
+			{
+				Type type = BatteryBlockEntity.InternalType.GetNestedType( BatteryBlockNetworkManagerClass, BindingFlags.Public | BindingFlags.NonPublic );
+				return type;
+			}
+		}
+
+		#endregion "Properties"
+
+		#region "Methods"
+
+		public static bool ReflectionUnitTest( )
+		{
+			try
+			{
+				bool result = true;
+
+				Type type = InternalType;
+				if ( type == null )
+					throw new Exception( "Could not find internal type for BatteryBlockNetworkManager" );
+				result &= BaseObject.HasMethod( type, BatteryBlockNetManagerBroadcastProducerEnabledMethod );
+				result &= BaseObject.HasMethod( type, BatteryBlockNetManagerBroadcastCurrentStoredPowerMethod );
+				result &= BaseObject.HasMethod( type, BatteryBlockNetManagerBroadcastSemiautoEnabledMethod );
+
+				Type packetType = InternalType.GetNestedType( BatteryBlockNetManagerCurrentStoredPowerPacketClass, BindingFlags.Public | BindingFlags.NonPublic );
+				result &= BaseObject.HasMethod( packetType, BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod );
+				result &= BaseObject.HasField( packetType, BatteryBlockNetManagerCurrentStoredPowerPacketValueField );
+				//				result &= BaseObject.HasField(packetType, BatteryBlockNetManagerCurrentStoredPowerPacketGetIdField);
+
+				Type refPacketType = packetType.MakeByRefType( );
+
+				return result;
+			}
+			catch ( Exception ex )
+			{
+				Console.WriteLine( ex );
+				return false;
+			}
+		}
+
+		public void BroadcastProducerEnabled( )
+		{
+			BaseObject.InvokeEntityMethod( BackingObject, BatteryBlockNetManagerBroadcastProducerEnabledMethod, new object[ ] { m_parent.ProducerEnabled } );
+		}
+
+		public void BroadcastCurrentStoredPower( )
+		{
+			BaseObject.InvokeEntityMethod( BackingObject, BatteryBlockNetManagerBroadcastCurrentStoredPowerMethod, new object[ ] { m_parent.CurrentStoredPower } );
+		}
+
+		public void BroadcastSemiautoEnabled( )
+		{
+			BaseObject.InvokeEntityMethod( BackingObject, BatteryBlockNetManagerBroadcastSemiautoEnabledMethod, new object[ ] { m_parent.SemiautoEnabled } );
+		}
+
+		protected static void RegisterPacketHandlers( )
+		{
+			try
+			{
+				if ( m_isRegistered )
+					return;
+
+				Type packetType = InternalType.GetNestedType( BatteryBlockNetManagerCurrentStoredPowerPacketClass, BindingFlags.Public | BindingFlags.NonPublic );
+				MethodInfo method = typeof( BatteryBlockNetworkManager ).GetMethod( "ReceiveCurrentPowerPacket", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static );
+				bool result = NetworkManager.RegisterCustomPacketHandler( PacketRegistrationType.Static, packetType, method, InternalType );
+				if ( !result )
+					return;
+
+				m_isRegistered = true;
+			}
+			catch ( Exception ex )
+			{
+				LogManager.ErrorLog.WriteLine( ex );
+			}
+		}
+
+		protected static void ReceiveCurrentPowerPacket<T>( ref T packet, Object netManager ) where T : struct
+		{
+			try
+			{
+				object result = BaseObject.InvokeEntityMethod( packet, BatteryBlockNetManagerCurrentStoredPowerPacketGetIdMethod );
+				//object result = BaseObject.GetEntityFieldValue(packet, BatteryBlockNetManagerCurrentStoredPowerPacketGetIdField);
+				if ( result == null )
+					return;
+				long entityId = (long)result;
+				BaseObject matchedEntity = GameEntityManager.GetEntity( entityId );
+				if ( matchedEntity == null )
+					return;
+				if ( !( matchedEntity is BatteryBlockEntity ) )
+					return;
+				BatteryBlockEntity battery = (BatteryBlockEntity)matchedEntity;
+
+				result = BaseObject.GetEntityFieldValue( packet, BatteryBlockNetManagerCurrentStoredPowerPacketValueField );
+				if ( result == null )
+					return;
+				float packetPowerLevel = (float)result;
+				if ( packetPowerLevel == 1.0f )
+					return;
+
+				BaseObject.SetEntityFieldValue( packet, BatteryBlockNetManagerCurrentStoredPowerPacketValueField, battery.CurrentStoredPower );
+
+				Type refPacketType = packet.GetType( ).MakeByRefType( );
+				MethodInfo basePacketHandlerMethod = BaseObject.GetStaticMethod( InternalType, BatteryBlockNetManagerCurrentPowerPacketReceiver, new Type[ ] { refPacketType, netManager.GetType( ) } );
+				basePacketHandlerMethod.Invoke( null, new object[ ] { packet, netManager } );
+			}
+			catch ( Exception ex )
+			{
+				LogManager.ErrorLog.WriteLine( ex );
+			}
+		}
+
+		#endregion "Methods"
+	}
+}
