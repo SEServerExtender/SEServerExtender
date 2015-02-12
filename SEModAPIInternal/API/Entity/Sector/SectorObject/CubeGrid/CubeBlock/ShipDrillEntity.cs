2a7c0eae9d7ad7eeb3b488be9205d097d1dbfdcb
dodexahedron <brandon@dodecatec.com>
2015-02-12 05:08:33 -0500

    Language-standard naming.
    Removed regions.
    Removed an unused local variable.

--- SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/ShipDrillEntity.cs
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/ShipDrillEntity.cs
@@ -6,65 +6,37 @@ using SEModAPIInternal.Support;
 
 namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
 {
-	[DataContract( Name = "ShipDrillEntityProxy" )]
+	[DataContract]
 	public class ShipDrillEntity : FunctionalBlockEntity
 	{
-		#region "Attributes"
-
-		private InventoryEntity m_inventory;
-
 		public static string ShipDrillNamespace = "Sandbox.Game.Weapons";
 		public static string ShipDrillClass = "MyShipDrill";
 
 		public static string ShipDrillGetInventoryMethod = "GetInventory";
 
-		#endregion "Attributes"
-
-		#region "Constructors and Intializers"
 
 		public ShipDrillEntity( CubeGridEntity parent, MyObjectBuilder_Drill definition )
 			: base( parent, definition )
 		{
-			m_inventory = new InventoryEntity( definition.Inventory );
+			Inventory = new InventoryEntity( definition.Inventory );
 		}
 
 		public ShipDrillEntity( CubeGridEntity parent, MyObjectBuilder_Drill definition, Object backingObject )
 			: base( parent, definition, backingObject )
 		{
-			m_inventory = new InventoryEntity( definition.Inventory, InternalGetContainerInventory( ) );
+			Inventory = new InventoryEntity( definition.Inventory, InternalGetContainerInventory( ) );
 		}
-
-		#endregion "Constructors and Intializers"
-
-		#region "Properties"
 
 		[DataMember]
 		[Category( "Drill" )]
 		[Browsable( false )]
 		[ReadOnly( true )]
-		public InventoryEntity Inventory
-		{
-			get
-			{
-				return m_inventory;
-			}
-			private set
-			{
-				//Do nothing!
-			}
-		}
-
-		#endregion "Properties"
-
-		#region "Methods"
-
-		#region "Internal"
+		public InventoryEntity Inventory { get; private set; }
 
 		protected Object InternalGetContainerInventory( )
 		{
 			try
 			{
-				Object baseObject = BackingObject;
 				Object actualObject = GetActualObject( );
 				Object inventory = InvokeEntityMethod( actualObject, ShipDrillGetInventoryMethod, new object[ ] { 0 } );
 
@@ -76,9 +48,5 @@ namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
 				return null;
 			}
 		}
-
-		#endregion "Internal"
-
-		#endregion "Methods"
 	}
 }
