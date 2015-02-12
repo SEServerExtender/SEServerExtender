c0506c09107ba13f67c324486c9022d4cded272a
dodexahedron <brandon@dodecatec.com>
2015-02-12 02:24:15 -0500

    Using fewer magic numbers, now that sectors can be infinite.
    Random doesn't need a manual time seed, as it is already time-seeded by default.
    Took care of the loop todo.

--- SEModAPIExtensions/API/CargoShipManager.cs
+++ SEModAPIExtensions/API/CargoShipManager.cs
@@ -12,20 +12,22 @@ using VRageMath;
 
 namespace SEModAPIExtensions.API
 {
+	using System.Linq;
+
 	public class CargoShipManager
 	{
-		private static CargoShipManager m_instance;
+		private static CargoShipManager _instance;
 
 		protected CargoShipManager( )
 		{
-			m_instance = this;
+			_instance = this;
 
 			Console.WriteLine( "Finished loading CargoShipManager" );
 		}
 
 		public static CargoShipManager Instance
 		{
-			get { return m_instance ?? ( m_instance = new CargoShipManager( ) ); }
+			get { return _instance ?? ( _instance = new CargoShipManager( ) ); }
 		}
 
 		public void SpawnCargoShipGroup( ulong remoteUserId = 0 )
@@ -35,14 +37,14 @@ namespace SEModAPIExtensions.API
 
 		public void SpawnCargoShipGroup( bool spawnAtAsteroids = true, ulong remoteUserId = 0 )
 		{
-			double worldSize = SandboxGameAssemblyWrapper.Instance.GetServerConfig( ).SessionSettings.WorldSizeKm * 1000.0f;
-			double spawnSize = 0.25f * worldSize;
-			double destinationSize = 0.02f * spawnSize;
+			double worldSize = SandboxGameAssemblyWrapper.Instance.GetServerConfig( ).SessionSettings.WorldSizeKm * 1000.0d;
+			double spawnSize = 0.25d * worldSize;
+			double destinationSize = 0.02d * spawnSize;
 
 			if ( spawnAtAsteroids )
 			{
 				double farthestAsteroidDistance = 0;
-				double nearestAsteroidDistance = 999999;
+				double nearestAsteroidDistance = double.MaxValue;
 				foreach ( VoxelMap voxelMap in SectorObjectManager.Instance.GetTypedInternalData<VoxelMap>( ) )
 				{
 					Vector3D asteroidPositon = voxelMap.Position;
@@ -52,8 +54,8 @@ namespace SEModAPIExtensions.API
 						nearestAsteroidDistance = asteroidPositon.Length( );
 				}
 
-				spawnSize = farthestAsteroidDistance * 2 + 10000;
-				destinationSize = nearestAsteroidDistance * 2 + 2000;
+				spawnSize = farthestAsteroidDistance * 2d + 10000d;
+				destinationSize = nearestAsteroidDistance * 2d + 2000d;
 			}
 
 			Vector3D groupPosition = UtilityFunctions.GenerateRandomBorderPosition( new Vector3D( -spawnSize, -spawnSize, -spawnSize ), new Vector3D( spawnSize, spawnSize, spawnSize ) );
@@ -67,8 +69,8 @@ namespace SEModAPIExtensions.API
 			try
 			{
 				//Calculate lowest and highest frequencies
-				float lowestFrequency = 999999;
-				float highestFrequency = 0;
+				double lowestFrequency = double.MaxValue;
+				double highestFrequency = 0d;
 				foreach ( MySpawnGroupDefinition entry in MyDefinitionManager.Static.GetSpawnGroupDefinitions( ) )
 				{
 					if ( entry.Frequency < lowestFrequency )
@@ -76,21 +78,14 @@ namespace SEModAPIExtensions.API
 					if ( entry.Frequency > highestFrequency )
 						highestFrequency = entry.Frequency;
 				}
-				if ( lowestFrequency <= 0 )
-					lowestFrequency = 1;
+				if ( lowestFrequency <= 0d )
+					lowestFrequency = 1d;
 
 				//Get a list of which groups *could* spawn
-				Random random = new Random( (int)DateTime.Now.ToBinary( ) );
+				Random random = new Random( );
 				double randomChance = random.NextDouble( );
 				randomChance = randomChance * ( highestFrequency / lowestFrequency );
-				List<MySpawnGroupDefinition> possibleGroups = new List<MySpawnGroupDefinition>( );
-				foreach ( MySpawnGroupDefinition entry in MyDefinitionManager.Static.GetSpawnGroupDefinitions( ) )
-				{
-					if ( entry.Frequency >= randomChance )
-					{
-						possibleGroups.Add( entry );
-					}
-				}
+				List<MySpawnGroupDefinition> possibleGroups = MyDefinitionManager.Static.GetSpawnGroupDefinitions( ).Where( entry => entry.Frequency >= randomChance ).ToList( );
 
 				//Determine which group *will* spawn
 				randomChance = random.NextDouble( );
@@ -103,58 +98,50 @@ namespace SEModAPIExtensions.API
 				Matrix orientation = Matrix.CreateLookAt( startPosition, stopPosition, new Vector3( 0, 1, 0 ) );
 				foreach ( MySpawnGroupDefinition.SpawnGroupPrefab entry in randomSpawnGroup.Prefabs )
 				{
-					MyPrefabDefinition matchedPrefab = null;
-					foreach ( var prefabEntry in MyDefinitionManager.Static.GetPrefabDefinitions( ) )
-					{
-						MyPrefabDefinition prefabDefinition = prefabEntry.Value;
-						if ( prefabDefinition.Id.SubtypeId.ToString( ) == entry.SubtypeId )
-						{
-							matchedPrefab = prefabDefinition;
-							break;
-						}
-					}
+					MyPrefabDefinition matchedPrefab = MyDefinitionManager.Static.GetPrefabDefinitions( ).Select( prefabEntry => prefabEntry.Value ).FirstOrDefault( prefabDefinition => prefabDefinition.Id.SubtypeId.ToString( ) == entry.SubtypeId );
 					if ( matchedPrefab == null )
 						continue;
 
-					//TODO - Build this to iterate through all cube grids in the prefab
-					MyObjectBuilder_CubeGrid objectBuilder = matchedPrefab.CubeGrids[ 0 ];
-
-					//Create the ship
-					CubeGridEntity cubeGrid = new CubeGridEntity( objectBuilder );
-
-					//Set the ship position and orientation
-					Vector3 shipPosition = Vector3.Transform( entry.Position, orientation ) + startPosition;
-					orientation.Translation = shipPosition;
-					MyPositionAndOrientation newPositionOrientation = new MyPositionAndOrientation( orientation );
-					cubeGrid.PositionAndOrientation = newPositionOrientation;
-
-					//Set the ship velocity
-					//Speed is clamped between 1.0f and the max cube grid speed
-					Vector3 travelVector = stopPosition - startPosition;
-					travelVector.Normalize( );
-					Vector3 shipVelocity = travelVector * Math.Min( cubeGrid.MaxLinearVelocity, Math.Max( 1.0f, entry.Speed ) );
-					cubeGrid.LinearVelocity = shipVelocity;
-
-					cubeGrid.IsDampenersEnabled = false;
-
-					foreach ( MyObjectBuilder_CubeBlock cubeBlock in cubeGrid.BaseCubeBlocks )
+					foreach ( MyObjectBuilder_CubeGrid objectBuilder in matchedPrefab.CubeGrids )
 					{
-						//Set the beacon names
-						if ( cubeBlock.TypeId == typeof( MyObjectBuilder_Beacon ) )
+
+						//Create the ship
+						CubeGridEntity cubeGrid = new CubeGridEntity( objectBuilder );
+
+						//Set the ship position and orientation
+						Vector3 shipPosition = Vector3.Transform( entry.Position, orientation ) + startPosition;
+						orientation.Translation = shipPosition;
+						MyPositionAndOrientation newPositionOrientation = new MyPositionAndOrientation( orientation );
+						cubeGrid.PositionAndOrientation = newPositionOrientation;
+
+						//Set the ship velocity
+						//Speed is clamped between 1.0f and the max cube grid speed
+						Vector3 travelVector = stopPosition - startPosition;
+						travelVector.Normalize( );
+						Vector3 shipVelocity = travelVector * Math.Min( cubeGrid.MaxLinearVelocity, Math.Max( 1.0f, entry.Speed ) );
+						cubeGrid.LinearVelocity = shipVelocity;
+
+						cubeGrid.IsDampenersEnabled = false;
+
+						foreach ( MyObjectBuilder_CubeBlock cubeBlock in cubeGrid.BaseCubeBlocks )
 						{
-							MyObjectBuilder_Beacon beacon = (MyObjectBuilder_Beacon)cubeBlock;
-							beacon.CustomName = entry.BeaconText;
+							//Set the beacon names
+							if ( cubeBlock.TypeId == typeof( MyObjectBuilder_Beacon ) )
+							{
+								MyObjectBuilder_Beacon beacon = (MyObjectBuilder_Beacon)cubeBlock;
+								beacon.CustomName = entry.BeaconText;
+							}
+
+							//Set the owner of every block
+							//TODO - Find out if setting to an arbitrary non-zero works for this
+							//cubeBlock.Owner = PlayerMap.Instance.GetServerVirtualPlayerId();
+							cubeBlock.Owner = 0;
+							cubeBlock.ShareMode = MyOwnershipShareModeEnum.Faction;
 						}
 
-						//Set the owner of every block
-						//TODO - Find out if setting to an arbitrary non-zero works for this
-						//cubeBlock.Owner = PlayerMap.Instance.GetServerVirtualPlayerId();
-						cubeBlock.Owner = 0;
-						cubeBlock.ShareMode = MyOwnershipShareModeEnum.Faction;
+						//And add the ship to the world
+						SectorObjectManager.Instance.AddEntity( cubeGrid );
 					}
-
-					//And add the ship to the world
-					SectorObjectManager.Instance.AddEntity( cubeGrid );
 				}
 
 				ChatManager.Instance.SendPrivateChatMessage( remoteUserId, string.Format( "Cargo group '{0}' spawned with {1} ships at {2}", randomSpawnGroup.DisplayNameText, randomSpawnGroup.Prefabs.Count, startPosition ) );
