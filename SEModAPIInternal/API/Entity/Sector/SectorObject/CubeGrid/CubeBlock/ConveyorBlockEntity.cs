4ef73199f05e9f2551034c0addcac20e262b0b66
dodexahedron <brandon@dodecatec.com>
2015-02-14 14:27:33 -0500

    Got rid of useless regions and the renamed datacontract type.

--- SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/ConveyorBlockEntity.cs
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/ConveyorBlockEntity.cs
@@ -4,17 +4,11 @@ using Sandbox.Common.ObjectBuilders;
 
 namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
 {
-	[DataContract( Name = "ConveyorBlockEntityProxy" )]
+	[DataContract]
 	public class ConveyorBlockEntity : CubeBlockEntity
 	{
-		#region "Attributes"
-
 		public static string ConveyorBlockNamespace = "5BCAC68007431E61367F5B2CF24E2D6F";
 		public static string ConveyorBlockClass = "D995E2F0C72D87529534369061DB78CF";
-
-		#endregion "Attributes"
-
-		#region "Constructors and Intializers"
 
 		public ConveyorBlockEntity( CubeGridEntity parent, MyObjectBuilder_Conveyor definition )
 			: base( parent, definition )
@@ -25,7 +19,5 @@ namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
 			: base( parent, definition, backingObject )
 		{
 		}
-
-		#endregion "Constructors and Intializers"
 	}
 }
