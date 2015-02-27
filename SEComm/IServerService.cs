65a22390fd5fa1dcd4ab0c7e1259fc946d766d31
dodexahedron <brandon@dodecatec.com>
2015-02-27 02:59:29 -0500

    xml-doc comments

--- SEComm/IServerService.cs
+++ SEComm/IServerService.cs
@@ -7,10 +7,25 @@
 	[ServiceContract]
 	public interface IServerService
 	{
+		/// <summary>
+		/// Attempts to start the server and returns basic information about the server.
+		/// </summary>
+		/// <param name="request">A request including, most importantly, the configuration name of the SE instance to start.</param>
+		/// <returns></returns>
 		[OperationContract]
 		StartServerResponse StartServer( StartServerRequest request );
+		
+		/// <summary>
+		/// Attempts to stop the currently running SE instance.
+		/// </summary>
 		[OperationContract]
 		void StopServer( );
+
+		/// <summary>
+		/// Immediately un-gracefully shuts the application down.
+		/// </summary>
+		/// <param name="exitCode">An exit code to attempt to return to the operating system.</param>
+		/// <remarks>Since SESE doesn't generally close all that cleanly anyway, the exit code probably won't work and you'll probably get an exception.</remarks>
 		[OperationContract]
 		void Exit( int exitCode );
 
