f2ed8c6617430698fba303ec79775c958bbe3713
dodexahedron <brandon@dodecatec.com>
2015-03-19 10:24:32 -0400

    Dispose of registry keys after we're done with them.
    Don't throw unnecessary internal exception types.
    Log when game path was blank.

--- SEModAPI/API/GameInstallation.cs
+++ SEModAPI/API/GameInstallation.cs
@@ -13,11 +13,10 @@ namespace SEModAPI.API
 	/// </summary>
 	public class GameInstallationInfo
 	{
-		#region "Attributes"
 
 		private static string _gamePath;
 
-		internal static readonly string[] CoreSpaceEngineersFiles = 
+		internal static readonly string[ ] CoreSpaceEngineersFiles = 
 		{
 			"Sandbox.Common.dll",
 			"Sandbox.Common.XmlSerializers.dll",
@@ -26,7 +25,8 @@ namespace SEModAPI.API
 			"VRage.Math.dll"
 		};
 
-		#endregion
+		private static readonly NLog.Logger BaseLog = NLog.LogManager.GetLogger( "BaseLog" );
+
 
 		#region "Constructor and Initializers"
 
@@ -37,39 +37,41 @@ namespace SEModAPI.API
 		/// the API will not be able to work
 		/// </para>
 		/// </summary>
-		public GameInstallationInfo()
+		public GameInstallationInfo( )
 		{
-			_gamePath = GetGameRegistryPath();
-			if (string.IsNullOrEmpty( _gamePath ))
+			_gamePath = GetGameRegistryPath( );
+			if ( string.IsNullOrEmpty( _gamePath ) )
 			{
-				_gamePath = GetGameSteamPath();
-				if (string.IsNullOrEmpty( _gamePath ))
+				_gamePath = GetGameSteamPath( );
+				if ( string.IsNullOrEmpty( _gamePath ) )
 				{
-					_gamePath = GetGameExePath();
-					if (string.IsNullOrEmpty( _gamePath ))
+					_gamePath = GetGameExePath( );
+					if ( string.IsNullOrEmpty( _gamePath ) )
 					{
-						throw new GameInstallationInfoException(GameInstallationInfoExceptionState.EmptyGamePath, "Can't find the game path");
+						throw new GameInstallationInfoException( GameInstallationInfoExceptionState.EmptyGamePath, "Can't find the game path" );
 					}
 				}
-					
+
 			}
 
-			if (!IsValidGamePath(_gamePath))
-				throw new GameInstallationInfoException(GameInstallationInfoExceptionState.BrokenGameDirectory, "The game directory is broken");
+			if ( !IsValidGamePath( _gamePath ) )
+				throw new GameInstallationInfoException( GameInstallationInfoExceptionState.BrokenGameDirectory, "The game directory is broken" );
 		}
 
 		/// <summary>
 		/// Create a new instance of GameInstallationInfo with the specified game location
 		/// </summary>
 		/// <param name="gamePath">Location of the game executable</param>
-		public GameInstallationInfo(string gamePath)
+		/// <exception cref="ArgumentNullException">The value of '<paramref name="gamePath"/>' cannot be null or empty.</exception>
+		/// <exception cref="FileNotFoundException">The gamePath provided is invalid</exception>
+		public GameInstallationInfo( string gamePath )
 		{
 			_gamePath = gamePath;
-			if (string.IsNullOrEmpty( _gamePath ))
-				throw new GameInstallationInfoException(GameInstallationInfoExceptionState.EmptyGamePath, "The gamePath given is empty");
+			if ( string.IsNullOrEmpty( _gamePath ) )
+				throw new ArgumentNullException( "gamePath", "The gamePath given is empty" );
 
-			if (!IsValidGamePath(_gamePath))
-				throw new GameInstallationInfoException(GameInstallationInfoExceptionState.BrokenGameDirectory, "The gamePath provided is invalid");
+			if ( !IsValidGamePath( _gamePath ) )
+				throw new FileNotFoundException( "The gamePath provided is invalid", gamePath );
 		}
 
 		#endregion
@@ -85,24 +87,26 @@ namespace SEModAPI.API
 
 		#region "Methods"
 
-		public static bool IsValidGamePath(string gamePath)
+		public static bool IsValidGamePath( string gamePath )
 		{
-			if (string.IsNullOrEmpty(gamePath))
+			if ( string.IsNullOrEmpty( gamePath ) )
+			{
+				BaseLog.Error( "Game path null or empty" );
+				return false;
+			}
+
+			if ( !Directory.Exists( gamePath ) )
+			{
+				BaseLog.Error( "Directory does not exist: {0}", gamePath );
+				return false;
+			}
+
+			if ( !Directory.Exists( Path.Combine( gamePath, "Content" ) ) )
 			{
 				return false;
 			}
 
-			if (!Directory.Exists(gamePath))
-			{
-				return false;
-			}
-
-			if (!Directory.Exists(Path.Combine(gamePath, "Content")))
-			{
-				return false;
-			}
-
-			if (!Directory.Exists(Path.Combine(gamePath, "DedicatedServer64")))
+			if ( !Directory.Exists( Path.Combine( gamePath, "DedicatedServer64" ) ) )
 			{
 				return false;
 			}
@@ -115,20 +119,21 @@ namespace SEModAPI.API
 		/// "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers"
 		/// </summary>
 		/// <returns>The absolute path to the game installation</returns>
-		public static string GetGameRegistryPath()
+		public static string GetGameRegistryPath( )
 		{
 			RegistryKey key;
-			if (Environment.Is64BitProcess)
-				key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244850", false);
+			if ( Environment.Is64BitProcess )
+				key = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244850", false );
 			else
-				key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244850", false);
+				key = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244850", false );
 
 			if ( key == null )
 			{
 				return null;
 			}
-			string path = key.GetValue("InstallLocation") as string;
-			return path != null && Directory.Exists(path) ? path : null;
+			string path = key.GetValue( "InstallLocation" ) as string;
+			key.Dispose( );
+			return path != null && Directory.Exists( path ) ? path : null;
 		}
 
 		/// <summary>
@@ -136,20 +141,21 @@ namespace SEModAPI.API
 		/// "C:\Program Files (x86)\Steam"
 		/// </summary>
 		/// <returns>Return the Steam install location, or null if not found</returns>
-		public static string GetGameSteamPath()
+		public static string GetGameSteamPath( )
 		{
 			RegistryKey key;
 
-			if (Environment.Is64BitProcess)
-				key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Valve\Steam", false);
+			if ( Environment.Is64BitProcess )
+				key = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Wow6432Node\Valve\Steam", false );
 			else
-				key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Valve\Steam", false);
+				key = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Valve\Steam", false );
 
-			if (key != null)
+			if ( key != null )
 			{
-				string steamBasePath = (string)key.GetValue("InstallPath");
-				string path = Path.Combine(steamBasePath, "steamapps", "common", "spaceengineers");
-				return Directory.Exists(path) ? path : null;
+				string steamBasePath = (string)key.GetValue( "InstallPath" );
+				key.Dispose( );
+				string path = Path.Combine( steamBasePath, "steamapps", "common", "spaceengineers" );
+				return Directory.Exists( path ) ? path : null;
 			}
 
 			return null;
@@ -159,49 +165,49 @@ namespace SEModAPI.API
 		/// Looks for the game install by going to the parent directory of this application
 		/// </summary>
 		/// <returns>The parent path of this application</returns>
-		public static string GetGameExePath()
+		public static string GetGameExePath( )
 		{
 			try
 			{
-				string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-				UriBuilder uri = new UriBuilder(codeBase);
-				string path = Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
-				DirectoryInfo directory = new DirectoryInfo(path);
+				string codeBase = Assembly.GetExecutingAssembly( ).CodeBase;
+				UriBuilder uri = new UriBuilder( codeBase );
+				string path = Path.GetDirectoryName( Uri.UnescapeDataString( uri.Path ) );
+				DirectoryInfo directory = new DirectoryInfo( path );
 				string finalPath = directory.Parent.FullName;
 
 				return finalPath;
 			}
-			catch (Exception)
+			catch ( Exception )
 			{
 				return "";
 			}
 		}
 
-		public bool IsBaseAssembliesChanged()
+		public bool IsBaseAssembliesChanged( )
 		{
 			// We use the Bin64 Path, as these assemblies are marked "All CPU", and will work regardless of processor architecture.
-			string baseFilePath = Path.Combine(GamePath, "DedicatedServer64");
-			string appFilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+			string baseFilePath = Path.Combine( GamePath, "DedicatedServer64" );
+			string appFilePath = Path.GetDirectoryName( Assembly.GetEntryAssembly( ).Location );
 
-			foreach (string filename in CoreSpaceEngineersFiles)
+			foreach ( string filename in CoreSpaceEngineersFiles )
 			{
-				if (DoFilesDiffer(baseFilePath, appFilePath, filename))
+				if ( DoFilesDiffer( baseFilePath, appFilePath, filename ) )
 					return true;
 			}
 
 			return false;
 		}
 
-		public bool UpdateBaseFiles()
+		public bool UpdateBaseFiles( )
 		{
 			// We use the Bin64 Path, as these assemblies are marked "All CPU", and will work regardless of processor architecture.
-			string baseFilePath = Path.Combine(GamePath, "DedicatedServer64");
+			string baseFilePath = Path.Combine( GamePath, "DedicatedServer64" );
 
-			foreach (string filename in CoreSpaceEngineersFiles)
+			foreach ( string filename in CoreSpaceEngineersFiles )
 			{
-				string sourceFile = Path.Combine(baseFilePath, filename);
+				string sourceFile = Path.Combine( baseFilePath, filename );
 
-				if (File.Exists(sourceFile))
+				if ( File.Exists( sourceFile ) )
 				{
 					//File.Copy(sourceFile, Path.Combine(appFilePath, filename), true);
 				}
@@ -210,55 +216,55 @@ namespace SEModAPI.API
 			return true;
 		}
 
-		public static bool DoFilesDiffer(string directory1, string directory2, string filename)
+		public static bool DoFilesDiffer( string directory1, string directory2, string filename )
 		{
-			return DoFilesDiffer(Path.Combine(directory1, filename), Path.Combine(directory2, filename));
+			return DoFilesDiffer( Path.Combine( directory1, filename ), Path.Combine( directory2, filename ) );
 		}
 
-		public static bool DoFilesDiffer(string file1, string file2)
+		public static bool DoFilesDiffer( string file1, string file2 )
 		{
-			if (File.Exists(file1) != File.Exists(file2))
+			if ( File.Exists( file1 ) != File.Exists( file2 ) )
 				return false;
 
-			byte[ ] buffer1 = File.ReadAllBytes(file1);
-			byte[ ] buffer2 = File.ReadAllBytes(file2);
+			byte[ ] buffer1 = File.ReadAllBytes( file1 );
+			byte[ ] buffer2 = File.ReadAllBytes( file2 );
 
-			if (buffer1.Length != buffer2.Length)
+			if ( buffer1.Length != buffer2.Length )
 				return true;
 
-			Assembly ass1 = Assembly.Load(buffer1);
+			Assembly ass1 = Assembly.Load( buffer1 );
 			Guid guid1 = ass1.ManifestModule.ModuleVersionId;
 
-			Assembly ass2 = Assembly.Load(buffer2);
+			Assembly ass2 = Assembly.Load( buffer2 );
 			Guid guid2 = ass2.ManifestModule.ModuleVersionId;
 
 			return guid1 != guid2;
 		}
 
-		internal static bool CheckIsRuningElevated()
+		internal static bool CheckIsRuningElevated( )
 		{
-			WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
-			return pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+			WindowsPrincipal pricipal = new WindowsPrincipal( WindowsIdentity.GetCurrent( ) );
+			return pricipal.IsInRole( WindowsBuiltInRole.Administrator );
 		}
 
-		internal static int? RunElevated(string fileName, string arguments, bool elevate, bool waitForExit)
+		internal static int? RunElevated( string fileName, string arguments, bool elevate, bool waitForExit )
 		{
-			ProcessStartInfo processInfo = new ProcessStartInfo(fileName, arguments);
+			ProcessStartInfo processInfo = new ProcessStartInfo( fileName, arguments );
 
-			if (elevate)
+			if ( elevate )
 			{
 				processInfo.Verb = "runas";
 			}
 
 			try
 			{
-				Process process = Process.Start(processInfo);
+				Process process = Process.Start( processInfo );
 
-				if (waitForExit)
+				if ( waitForExit )
 				{
-					if (process != null)
+					if ( process != null )
 					{
-						process.WaitForExit();
+						process.WaitForExit( );
 
 						return process.ExitCode;
 					}
@@ -266,7 +272,7 @@ namespace SEModAPI.API
 
 				return 0;
 			}
-			catch (Win32Exception)
+			catch ( Win32Exception )
 			{
 				// Do nothing. Probably the user canceled the UAC window
 				return null;
