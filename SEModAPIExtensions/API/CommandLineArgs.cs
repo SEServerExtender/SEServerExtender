5dba783182ff93f608274423c2a60df4ec0d0e74
dodexahedron <brandon@dodecatec.com>
2015-03-19 11:32:37 -0400

    Some intellisense.

--- SEModAPIExtensions/API/CommandLineArgs.cs
+++ SEModAPIExtensions/API/CommandLineArgs.cs
@@ -5,10 +5,15 @@ namespace SEModAPIExtensions.API
 		public bool AutoStart;
 		public string WorldName;
 		public string InstanceName;
+		/// <summary>Do not show the local GUI, if true.</summary>
 		public bool NoGui;
+		/// <summary>Do not show a command-line console, if true.</summary>
 		public bool NoConsole;
+		/// <summary>Output additional debugging log information.</summary>
 		public bool Debug;
+		/// <summary>The path to the game installation folder (parent of DedicatedServer64 folder).</summary>
 		public string GamePath;
+		/// <summary>Disable WCF services, if true.</summary>
 		public bool NoWcf;
 		public int Autosave;
 		public string Path;
