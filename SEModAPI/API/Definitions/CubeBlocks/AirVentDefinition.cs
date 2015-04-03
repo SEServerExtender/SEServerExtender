e64799d23c048fe6313ad5c266e1686bea89d969
dodexahedron <brandon@dodecatec.com>
2015-04-03 13:30:51 -0400

    Added preliminary AirVent support.

---
+++ SEModAPI/API/Definitions/CubeBlocks/AirVentDefinition.cs
@@ -0,0 +1,50 @@
+﻿namespace SEModAPI.API.Definitions.CubeBlocks
+{
+	using System.ComponentModel;
+	using Sandbox.Common.ObjectBuilders.Definitions;
+
+	class AirVentDefinition : BlockDefinition
+	{
+		public AirVentDefinition( MyObjectBuilder_CubeBlockDefinition definition ) : base( definition )
+		{
+		}
+
+		/// <summary>
+		/// The current ventilation capacity per second.
+		/// </summary>
+		[Browsable( true )]
+		[ReadOnly( true )]
+		[Description( "The current ventilation capacity per second." )]
+		public float VentilationCapacityPerSecond
+		{
+			get { return GetSubTypeDefinition( ).VentilationCapacityPerSecond; }
+		}
+
+		/// <summary>
+		/// The operational power consumption.
+		/// </summary>
+		[Browsable( true )]
+		[ReadOnly( true )]
+		[Description( "The operational power consumption." )]
+		public float OperationalPowerConsumption
+		{
+			get { return GetSubTypeDefinition( ).OperationalPowerConsumption; }
+		}
+
+		/// <summary>
+		/// The standby power consumption.
+		/// </summary>
+		[Browsable( true )]
+		[ReadOnly( true )]
+		[Description( "The standby power consumption." )]
+		public float StandbyPowerConsumption
+		{
+			get { return GetSubTypeDefinition( ).StandbyPowerConsumption; }
+		}
+
+		public new MyObjectBuilder_AirVentDefinition GetSubTypeDefinition( )
+		{
+			return (MyObjectBuilder_AirVentDefinition) BaseDefinition;
+		}
+	}
+}
---
+++ SEModAPIInternal/API/Entity/Sector/SectorObject/CubeGrid/CubeBlock/AirVentEntity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace SEModAPIInternal.API.Entity.Sector.SectorObject.CubeGrid.CubeBlock
+{
+	using System.ComponentModel;
+	using System.Runtime.Serialization;
+	using Sandbox.Common.ObjectBuilders;
+	using SEModAPIInternal.Support;
+
+	public class AirVentEntity : TerminalBlockEntity
+	{
+		public static string AirVentNamespace = "";
+		public static string AirVentClass = "=Ninmd8b0GgB8nNeFY3EhBjlmin=";
+
+		public static string AirVentGetOxygenLevelMethod = "GetOxygenLevel";
+		public static string AirVentIsPressurizedMethod = "IsPressurized";
+
+
+		public AirVentEntity( CubeGridEntity parent, MyObjectBuilder_AirVent definition )
+			: base( parent, definition )
+		{
+			
+		}
+
+		public AirVentEntity( CubeGridEntity parent, MyObjectBuilder_AirVent definition, object backingObject )
+			: base( parent, definition, backingObject )
+		{
+			
+		}
+
+		[IgnoreDataMember]
+		[Category( "Air Vent" )]
+		[Browsable( false )]
+		[ReadOnly( true )]
+		internal new MyObjectBuilder_AirVent ObjectBuilder
+		{
+			get
+			{
+				MyObjectBuilder_AirVent objectBuilder = (MyObjectBuilder_AirVent)base.ObjectBuilder;
+				if ( objectBuilder == null )
+				{
+					objectBuilder = new MyObjectBuilder_AirVent( );
+					ObjectBuilder = objectBuilder;
+				}
+
+				return objectBuilder;
+			}
+			set
+			{
+				base.ObjectBuilder = value;
+			}
+		}
+
+		/// <exception cref="System.Exception">Any exception is thrown when trying to invoke the GetOxygenLevel method.</exception>
+		public float GetOxygenLevel( )
+		{
+			try
+			{
+				object airVent = GetActualObject( );
+				return (float)InvokeEntityMethod( airVent, AirVentGetOxygenLevelMethod );
+			}
+			catch ( Exception e )
+			{
+				ApplicationLog.BaseLog.Error( e );
+				throw;
+			}
+		}
+
+		/// <exception cref="System.Exception">Any exception is thrown when trying to invoke the IsPressurized method.</exception>
+		public bool IsPressurized( )
+		{
+			try
+			{
+				object airVent = GetActualObject( );
+				return (bool) InvokeEntityMethod( airVent, AirVentIsPressurizedMethod );
+			}
+			catch ( Exception e )
+			{
+				ApplicationLog.BaseLog.Error( e );
+				throw;
+			}
+		}
+	}
+}
