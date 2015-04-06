2089f22742e20383ce29a3335b0965fe9adc0ff3
dodexahedron <brandon@dodecatec.com>
2015-04-05 23:31:31 -0400

    Added utility method for command parsing. Based on fccf28349f87ea5fd26e36592656797b1f4ba025 (https://github.com/SEServerExtender/EssentialsPlugin/pull/13)

---
+++ SEModAPI/API/Utility/CommandParser.cs
@@ -0,0 +1,50 @@
+﻿namespace SEModAPI.API.Utility
+{
+	using System.Collections.Generic;
+	using System.Text;
+	public static class CommandParser
+	{
+		public static List<string> GetCommandParts( string input )
+		{
+			bool quote = false;
+			List<string> output = new List<string>( );
+			StringBuilder sb = new StringBuilder( );
+			for ( int pos = 0; pos < input.Length; pos++ )
+			{
+				char character = input[ pos ];
+
+				if ( character == '"' )
+				{
+					if ( !quote )
+					{
+						quote = true;
+					}
+					else if ( pos + 1 < input.Length && input[ pos + 1 ] == '"' )
+					{
+						sb.Append( character );
+						pos++;
+					}
+					else
+					{
+						quote = false;
+					}
+				}
+				else if ( char.IsWhiteSpace( character ) && !quote )
+				{
+					output.Add( sb.ToString( ) );
+					sb.Clear( );
+				}
+				else
+				{
+					sb.Append( character );
+				}
+			}
+
+			if ( !quote && sb.Length > 0 )
+			{
+				output.Add( sb.ToString( ) );
+			}
+			return output;
+		}
+	}
+}
