8d10153525da7ed6fe2fe5f56ae86f810f17efa9
dodexahedron <brandon@dodecatec.com>
2015-06-05 21:26:53 -0400

    Add HasMethod to SEModAPI.

--- SEModAPI/API/Utility/Reflection.cs
+++ SEModAPI/API/Utility/Reflection.cs
@@ -56,5 +56,61 @@
 			}
 		}
 
+		public static bool HasMethod( Type objectType, string methodName )
+		{
+			return Reflection.HasMethod( objectType, methodName, null );
+		}
+
+
+		public static bool HasMethod( Type objectType, string methodName, Type[ ] argTypes )
+		{
+			try
+			{
+				if ( String.IsNullOrEmpty( methodName ) )
+					return false;
+
+				if ( argTypes == null )
+				{
+					MethodInfo method = objectType.GetMethod( methodName );
+					if ( method == null )
+						method = objectType.GetMethod( methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy );
+					if ( method == null && objectType.BaseType != null )
+						method = objectType.BaseType.GetMethod( methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy );
+					if ( method == null )
+					{
+						if ( ExtenderOptions.IsDebugging )
+							Log.Error( "Failed to find method '" + methodName + "' in type '" + objectType.FullName + "'" );
+						return false;
+					}
+				}
+				else
+				{
+					MethodInfo method = objectType.GetMethod( methodName, argTypes );
+					if ( method == null )
+						method = objectType.GetMethod( methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy, Type.DefaultBinder, argTypes, null );
+					if ( method == null && objectType.BaseType != null )
+						method = objectType.BaseType.GetMethod( methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy, Type.DefaultBinder, argTypes, null );
+					if ( method == null )
+					{
+						if ( ExtenderOptions.IsDebugging )
+							Log.Error( "Failed to find method '" + methodName + "' in type '" + objectType.FullName + "'" );
+						return false;
+					}
+				}
+
+				return true;
+			}
+			catch ( AmbiguousMatchException aex )
+			{
+				return true;
+			}
+			catch ( Exception ex )
+			{
+				if ( ExtenderOptions.IsDebugging )
+					Log.Error( "Failed to find method '" + methodName + "' in type '" + objectType.FullName + "': " + ex.Message );
+				Log.Error( ex );
+				return false;
+			}
+		}
 	}
 }
